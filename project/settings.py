c88981c1530f8dd5911425a85385c154e02b47bf
luciostuder <lucio.studer@gmail.com>
2025-11-06 16:06:26 +0000

    updated settings.py for deploy

--- project/settings.py
+++ project/settings.py
@@ -12,8 +12,15 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -133,3 +140,23 @@ MEDIA_ROOT = '/home/deisishop/project/media'
 MEDIA_URL = '/media/'
 STATIC_ROOT = '/home/deisishop/project/static'
 STATIC_URL = '/static/'
+
+
+
+## novo 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
+]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles') 
